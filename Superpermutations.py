d0355e83a071e2cc14b978ae7b8593b5fa22fd5b
TruiteAgitee <pelegrin.gregoire@gmail.com>
2021-02-22 11:44:16 +0100

    Adding NodeLib

--- Superpermutations.py
+++ Superpermutations.py
@@ -1,13 +1,18 @@
-from itertools import permutations
+from itertools import permutations as permutFunction
+
+import NodeLib as nl
 
 # TODO: Generate every permutation
-n = int(input("Number of elts: "))
+ORDER = int(input("Number of elts: "))
 # Generate the pool of elts from the number of elt
 pool = []
-for i in range(n):
+for i in range(ORDER):
 	pool.append(chr(65+i))
-perm = list(permutations(pool))
-print(perm)
+permutations = list(permutFunction(pool))
 
 # TODO: Make the graph
+graph = []
+for perm in permutations:
+	graph.append(nl.Node(perm))
+
 # TODO: A* algorithm in the graph
