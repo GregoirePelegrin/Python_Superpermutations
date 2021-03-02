2e233cdf19a8a65510cfa99ead4eac3ee93ed6e9
TruiteAgitee <pelegrin.gregoire@gmail.com>
2021-03-02 01:12:32 +0100

    Adding Nearest Insertion algorithm
    * Adding functions relative to Nearest Insertion algorithm
    * Made things simpler to test the program, varying the length of x wil propagate to the rest of the program

--- Superpermutations.py
+++ Superpermutations.py
@@ -1,6 +1,7 @@
 from alive_progress import alive_bar
 from itertools import permutations
 from matplotlib import pyplot as plt
+from sympy.ntheory import binomial_coefficients
 
 import LibSuperpermutation as sl
 
@@ -17,6 +18,51 @@ def generatePool(nbrElts):
 	for i in range(nbrElts):
 		temp.append(chr(65+i))
 	return temp
+def generateSuperpermutationFromGroup(group):
+	res = sl.Superpermutation()
+	for n in group:
+		res.include(n)
+	return res
+# Functions relative to the Nearest Insertion algorithm
+def NearestInsertion_findNearest(group, nodes):	# Not opti: doesn't take into account the direction of the proximity
+	minDist = group[0].distance(nodes[0])
+	minIndex = 0
+	minNode = nodes[0]
+	placeIndex = 1
+	for i1,n1 in enumerate(group):
+		for i2,n2 in enumerate(nodes):
+			d = n1.distance(n2)
+			if minDist > d:
+				minDist = d
+				minIndex = i2
+				minNode = n2
+				placeIndex = i1+1
+	return minNode, minIndex, placeIndex
+def NearestInsertion_getBestNearestInsertion(nodes):
+	temp = sl.Superpermutation()
+	if len(nodes) < 2:
+		return temp
+	for n in nodes:
+		temp.include(n)
+	minSuperp = temp
+	with alive_bar(binomial_coefficients(len(nodes))[(2, len(nodes)-2)], title="NearestInsertion", length=20) as bar:
+		for i in range(len(nodes)-1):
+			for j in range(i+1, len(nodes)):
+				temp = NearestInsertion_getCurrentNearestInsertion(nodes, [nodes[i], nodes[j]], minSuperp)
+				if minSuperp.length > temp.length:
+					minSuperp = temp
+				bar()
+	return minSuperp
+def NearestInsertion_getCurrentNearestInsertion(nodes, starters, minSuperp):
+	nodesList = nodes[:]
+	group = starters[:]
+	temp = generateSuperpermutationFromGroup(group)
+	while temp.length < minSuperp.length and len(nodesList) != 0:
+		nextNode, nextIndex, nextPlace = NearestInsertion_findNearest(group, nodesList)
+		group.insert(nextPlace, nextNode)
+		temp = generateSuperpermutationFromGroup(group)
+		del nodesList[nextIndex]
+	return temp
 # Functions relative to the Nearest Neighbour algorithm
 def NearestNeighbour_findNearest(node, nodes):
 	minDist = node.distance(nodes[0])
@@ -31,10 +77,10 @@ def NearestNeighbour_findNearest(node, nodes):
 	return minNode, minIndex
 def NearestNeighbour_getBestNearestNeighbour(nodes):
 	temp = sl.Superpermutation()
-	for n in graph:
+	for n in nodes:
 		temp.include(n)
 	minSuperp = temp
-	with alive_bar(len(nodes)) as bar:
+	with alive_bar(len(nodes), title="NearestNeighbour", length=20) as bar:
 		for starter in nodes:
 			temp = NearestNeighbour_getCurrentNearestNeighbour(nodes, starter, minSuperp)
 			if minSuperp.length > temp.length:
@@ -43,32 +89,41 @@ def NearestNeighbour_getBestNearestNeighbour(nodes):
 	return minSuperp
 def NearestNeighbour_getCurrentNearestNeighbour(nodes, starter, minSuperp):
 	nodesList = nodes[:]
-	temp = sl.Superpermutation().include(starter)
 	currentNode = starter
+	temp = sl.Superpermutation().include(currentNode)
 	while temp.length < minSuperp.length and len(nodesList) != 0:
 		nextNode, nextIndex = NearestNeighbour_findNearest(currentNode, nodesList)
 		temp.include(nextNode)
 		del nodesList[nextIndex]
 	return temp
 
-# ORDER = int(input("Number of elts: "))
+def comparison(maxOrder=5):
+	minLengths = {}
+	#for ORDER in range(1, 7):
+	for ORDER in range(1, 5):
+		print("ORDER = {}".format(ORDER))
+		# Generate the pool of elts from the number of elt
+		pool = generatePool(ORDER)
+		permutationList = list(permutations(pool))
+		graph = generateGraph(permutationList)
 
-minLengths = {}
-for ORDER in range(1, 7):
-	# Generate the pool of elts from the number of elt
-	pool = generatePool(ORDER)
-	permutationList = list(permutations(pool))
-	graph = generateGraph(permutationList)
+		# Implementing Nearest-Neighbour on the graph
+		superpermutation = NearestNeighbour_getBestNearestNeighbour(graph[:])
+		if "NearestNeighbour" not in minLengths:
+			minLengths["NearestNeighbour"] = []
+		minLengths["NearestNeighbour"].append(superpermutation.length)
 
-	# Implementing Nearest-Neighbour on the graph
-	superpermutation = NearestNeighbour_getBestNearestNeighbour(graph)
-	if "NearestNeighbour" not in minLengths:
-		minLengths["NearestNeighbour"] = []
-	minLengths["NearestNeighbour"].append(superpermutation.length)
+		# Implementing Nearest-Insertion on the graph
+		superpermutation = NearestInsertion_getBestNearestInsertion(graph[:])
+		if "NearestInsertion" not in minLengths:
+			minLengths["NearestInsertion"] = []
+		minLengths["NearestInsertion"].append(superpermutation.length)
+	return minLengths
 
-x = [i for i in range(1, 7)]
-yTheo = [1, 3, 9, 33, 153, 872]
+x = [i for i in range(1, 6)]
+yTheo = [1, 3, 9, 33, 153, 872, 5913, 46233, 409113, 4037913][:len(x)]
 plt.plot(x, yTheo, label="Theoretical lengths")
+minLengths = comparison(len(x))
 for key in minLengths:
 	plt.plot(x, minLengths[key], label=key)
 plt.legend()
